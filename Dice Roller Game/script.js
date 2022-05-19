121844ad3277a9df7c909d971d989356c37cb847
Manuraj Balakrishnan <42644522+ManurajBalakrishnan@users.noreply.github.com>
2022-05-19 16:55:58 +0530

    Update script.js

--- Dice Roller Game/script.js
+++ Dice Roller Game/script.js
@@ -1,11 +1,13 @@
 let button=document.querySelector(".btn");
 button.onclick=function players()
 {
-    let random1=Math.floor(Math.random()*6)+1;
-    document.querySelector(".Player1dice").setAttribute("src", `Images/dice${random1}.png`);
+    let random1=Math.floor(Math.random()*6)+1; //getting random number for player1
+    document.querySelector(".Player1dice").setAttribute("src", `Images/dice${random1}.png`); // Changing Player1 dice img
 
-    let random2=Math.floor(Math.random()*6)+1;
-    document.querySelector(".Player2dice").setAttribute("src", `Images/dice${random2}.png`);
+    let random2=Math.floor(Math.random()*6)+1; //getting random number for player2
+    document.querySelector(".Player2dice").setAttribute("src", `Images/dice${random2}.png`); // Changing Player2 dice img
+    
+    //Checking the win and draw condition
     if(random1 > random2)
     {
         document.querySelector(".result").innerHTML="Player 1 Won";
